fc8efd5a72a4aa54a6a28cab8eaa9a8799868d90
Denys Nyckson <denysnyckson@ufrn.edu.br>
2020-06-26 17:20:40 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+// Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+});
+
+app.listen(8080,()=>{console.log("App rodando!");});
